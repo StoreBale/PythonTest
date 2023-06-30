f229d9cf69eb2ccd665628999c0c79a8273c6523
Zscing <{ID}+{username}@users.noreply.github.com>
2023-06-30 17:27:34 +0800

    maze update2

--- maze.py
+++ maze.py
@@ -1,6 +1,5 @@
 import random
 
-# 定义迷宫地图
 maze = [
     ["1", "1", "1", "1", "1", "1", "1"],
     ["1", "0", "0", "0", "0", "1", "1"],
@@ -10,15 +9,13 @@ maze = [
     ["1", "0", "0", "1", "0", "0", "1"],
     ["1", "1", "1", "1", "1", "1", "1"]
 ]
-
-# 定义玩家位置
+#玩家開局位置
 player_position = [1, 1]
-
-# 定义终点位置
+#迷宮出口位置
 end_position = [5,5]
-# 游戏循环
+
 while True:
-    # 打印迷宫地图
+    
     for row_index,row in enumerate(maze):
         for cell_index,cell in enumerate(row):
             if row_index == player_position[0] and cell_index == player_position[1]:
@@ -29,10 +26,10 @@ while True:
                 print(cell, end=" ")
         print()
         
-    # 获取玩家输入
-    move = input("请输入移动方向（wasd）：")
     
-    # 更新玩家位置
+    move = input("請輸入移動方向（wasd）：")
+    
+    
     if move == "w":
         new_position = [player_position[0] - 1, player_position[1]]
     elif move == "a":
@@ -42,21 +39,19 @@ while True:
     elif move == "d":
         new_position = [player_position[0], player_position[1] + 1]
     else:
-        print("无效的移动方向！")
+        print("請使用正確方向鍵！")
         continue
     
-    # 检查新位置是否合法
+    
     if not (0 <= new_position[0] < len(maze) and 0 <= new_position[1] < len(maze[0])):
-        print("超出边界！")
+        print("超過地圖！")
         continue
     elif maze[new_position[0]][new_position[1]] == "1":
-        print("遇到1不能移动！")
+        print("撞牆了！")
         continue
     
-    # 更新玩家位置
     player_position = new_position
     
-    # 检查是否到达终点
     if player_position == end_position:
-        print("恭喜你，闯关成功！")
+        print("恭喜你，成功！")
         break
